c092baa91d84bfd2de3ada63de3cf6d25165458f
Fuma_Tsuchiya <korezonzip@gmail.comm>
2019-09-08 18:23:29 +0900

    途中

---
+++ src/main/scala/one/OneOne.scala
@@ -0,0 +1,13 @@
+
+import scala.collection.JavaConverters._
+
+// you can write to stdout for debugging purposes, e.g.
+// println("this is a debug message")
+
+object OneOne {
+  def solution(n: Int): Int = {
+    // write your code in Scala 2.12
+    //toBinaryString: 10進数 -> 2進数
+    val binaryNumber:String = n.toBinaryString
+  }
+}
