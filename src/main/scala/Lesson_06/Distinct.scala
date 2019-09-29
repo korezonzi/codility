064168a3c286cb29edb1fd4ab49d9f7e9b141d4e
fuma-tsuchiya <fuma.tsuchiya@nextbeat.net>
2019-09-29 11:21:01 +0900

    Done Thursday

---
+++ src/main/scala/Lesson_06/Distinct.scala
@@ -0,0 +1,10 @@
+import scala.collection.JavaConverters._
+
+// you can write to stdout for debugging purposes, e.g.
+// println("this is a debug message")
+
+object Distinct {
+  def solution(a: Array[Int]): Int = {
+    a.distinct.size
+  }
+}
